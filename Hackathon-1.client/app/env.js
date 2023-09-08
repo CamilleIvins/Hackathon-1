a00d7b2193cd7a84e1b00bc524af1af4d8bc3bdc
Camille Ivins <camillemivins@gmail.com>
2023-09-08 13:02:31 -0600

    retry env.js and .env

--- Hackathon-1.client/app/env.js
+++ Hackathon-1.client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-empex7cj2h0al138.us.auth0.com'
-export const audience = 'https://sandcastle/'
+export const domain = 'https://sandcastle/'
+export const audience = 'dev-empex7cj2h0al138.us.auth0.com'
 export const clientId = 'ZVL4bcvPZQLMBI358CDHdndwYiEn70mF'
